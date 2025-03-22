2ec786c2698eef58f820b8f1fedcb59ba6bbaa48
luilsons <luilsonms@hotmail.com>
2025-03-22 16:07:15 -0400

    Product function

--- ProductListViewController.swift
+++ ProductListViewController.swift
@@ -5,4 +5,16 @@
 //  Created by Luilson Marcos Sousa dos Reis on 2025-03-22.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+class ProductListViewController: UITableViewController {
+    
+    var products: [Product] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "All Products"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        fetchProducts()
+    }
